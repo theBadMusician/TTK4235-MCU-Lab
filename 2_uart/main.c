5566b8ed5b5ad736e19da33e24aa922dc698b6fe
Benjaminas Visockis <59376885+theBadMusician@users.noreply.github.com>
2026-03-24 23:48:52 +0100

    Add task 2.7 - scanf reader and return printf

--- 2_uart/main.c
+++ 2_uart/main.c
@@ -16,6 +16,16 @@ ssize_t _write(int fd, const void *buf, size_t count) {
   return count;
 }
 
+ssize_t _read(int fd, void *buf, size_t count){
+  char *str = (char *)(buf);
+  char letter;
+  do {
+    letter = uart_read();
+  } while(letter == '\0');
+  *str = letter;
+  return 1;
+}
+
 void button_init(){ 
   /* Configure buttons as inputs with pull-up resistors */
   for (uint8_t i_btn = 0; i_btn < 4; i_btn++) {
@@ -49,6 +59,18 @@ int main(){
     for (uint8_t i_btn = 0; i_btn < 4; i_btn++) {
       btn_states[i_btn] = (GPIO->IN & (1 << (BUTTON_1_PIN + i_btn))) == 0; // Active low
     }
+    
+    if (btn_states[0])  {GPIO->OUTCLR = (1 << 17);} // Turn on LED
+    else                {GPIO->OUTSET = (1 << 17);} // Turn off LED
+
+    if (btn_states[1])  {GPIO->OUTCLR = (0b01 << 18);}
+    else                {GPIO->OUTSET = (0b01 << 18);}
+
+    if (btn_states[2])  {GPIO->OUTCLR = (0b01 << 19);}
+    else                {GPIO->OUTSET = (0b01 << 19);}
+
+    if (btn_states[3])  {GPIO->OUTCLR = (0b01 << 20);}
+    else                {GPIO->OUTSET = (0b01 << 20);}
 
     if(btn_states[0] && !prev_btn_states[0]) { // Button 1 pressed
       uart_send('A');
@@ -65,11 +87,23 @@ int main(){
     }
     prev_btn_states[2] = btn_states[2];
 
-    if (btn_states[0])  {GPIO->OUTCLR = (1 << 17);} // Turn on LED
-    else                {GPIO->OUTSET = (1 << 17);} // Turn off LED
+    if(btn_states[3] && !prev_btn_states[3]) { // Button 4 pressed
+      iprintf("Enter two numbers separated by space: \n\r");
+      int32_t num1 = 0, num2 = 0;
+      uint8_t words_recvd = scanf("%ld %ld", &num1, &num2);
 
-    if (btn_states[1])  {GPIO->OUTCLR = (0b01 << 18);}
-    else                {GPIO->OUTSET = (0b01 << 18);}
+      if (words_recvd == 2) iprintf("Product of the numbers is: %ld\n\r", (num1 * num2));
+      else { 
+        iprintf("Invalid input. Please enter two numbers separated by space.\n\r");
+        // while(uart_read() != '\0');
+        int c;
+        while ((c = getchar()) != '\n' && c != '\r' && c != EOF) {
+            // Consume char
+        }
+      }
+    }
+    prev_btn_states[3] = btn_states[3];
+
 
     char recvd = uart_read();
     if (recvd != '\0') {
