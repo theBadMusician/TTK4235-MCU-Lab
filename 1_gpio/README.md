7eea4bc29c6d48c044afbac1fefd5cf3eeb0008a
Benjaminas Visockis <59376885+theBadMusician@users.noreply.github.com>
2026-03-24 15:37:23 +0100

    Add task 1 from the lab manual

---
+++ 1_gpio/README.md
@@ -0,0 +1,11 @@
+# Lab 3 Oppgave 1 - GPIO
+
+## Opgave 1.2
+
+If the shield is not present, the buttons are connected directly to pins labeled BUTTON[1-4]. They are mapped to the GPIO pins on the target MCU specified as P0.1[3-6].
+
+The buttons are active low, meaning that when a button is pressed, it connects the corresponding GPIO pin to ground (GND), resulting in a low signal.
+
+
+
+
--- 1_gpio/main.c
+++ 1_gpio/main.c
@@ -1,6 +1,12 @@
 #include <stdint.h>
+#include <stdbool.h>
 
-#define GPIO ((NRF_GPIO_REGS*)__GPIO_BASE_ADDRESS__)
+#define __GPIO_BASE_ADDRESS__ 0x50000000
+#define __BUTTON_1_PIN__ 13
+#define __BUTTON_2_PIN__ 14
+
+// Reserved 1 between 0x524 and 0x700 minus DETECTMODE -> 0x700 - 0x528 = 472 bytes -> 472 / 4 = 118 registers
+#define __RESERVED1_SIZE__ 118
 
 typedef struct {
 	volatile uint32_t RESERVED0[321];
@@ -17,9 +23,12 @@ typedef struct {
 	volatile uint32_t PIN_CNF[32];
 } NRF_GPIO_REGS;
 
+#define GPIO ((NRF_GPIO_REGS*)__GPIO_BASE_ADDRESS__)
+
+
 void button_init(){ 
-	GPIO->PIN_CNF[__BUTTON_1_PIN__] = (3 << 2);
-	// Fill inn the configuration for the remaining buttons 
+	GPIO->PIN_CNF[__BUTTON_1_PIN__] = (3 << 2); // Configure button 1 as input with pull-up resistor
+  GPIO->PIN_CNF[__BUTTON_2_PIN__] = (3 << 2); // Configure button 2 as input with pull-up resistor
 }
 
 int main(){
@@ -27,18 +36,39 @@ int main(){
 	for(int i = 17; i <= 20; i++){
 		GPIO->DIRSET = (1 << i);
 		GPIO->OUTCLR = (1 << i);
+
+    // GPIO->OUTSET = (1 << i); // Set pins 17-20 high to turn off LED matrix (active low)
 	}
 
 	// Configure buttons -> see button_init()
+  button_init();
 
-	int sleep = 0;
+	volatile int sleep = 0;
+
+  bool btn1_state = false;
+  bool btn2_state = false;
+  bool prev_btn1_state = false;
+  bool prev_btn2_state = false;
+
 	while(1){
 
 		/* Check if button 1 is pressed;
 		 * turn on LED matrix if it is. */
+    btn1_state = (GPIO->IN & (1 << __BUTTON_1_PIN__)) == 0; // Active low
+
+    if(btn1_state && !prev_btn1_state) { // Button 1 pressed
+        GPIO->OUTCLR = (0xF << 17); // Turn on LED matrix (pins 17-20)
+    }
+    prev_btn1_state = btn1_state;
 
 		/* Check if button 2 is pressed;
 		 * turn off LED matrix if it is. */
+    btn2_state = (GPIO->IN & (1 << __BUTTON_2_PIN__)) == 0; // Active low
+
+    if(btn2_state && !prev_btn2_state) { // Button 2 pressed
+        GPIO->OUTSET = (0xF << 17); // Turn off LED matrix (pins 17-20)
+    }
+    prev_btn2_state = btn2_state; 
 
 		sleep = 10000;
 		while(--sleep); // Delay
