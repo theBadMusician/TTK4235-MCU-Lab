1aaf8ce37a7935eadd428345367537a91c4fad8a
Benjaminas Visockis <59376885+theBadMusician@users.noreply.github.com>
2026-03-25 22:30:25 +0100

    Refactor ppi.h according to the nRF52832 TRM

--- 3_gpiote/ppi.h
+++ 3_gpiote/ppi.h
@@ -15,17 +15,21 @@ typedef struct {
 } PPI_CHANNEL_TYPE;
 
 typedef struct {
-	// Tasks
-	volatile PPI_TASKS_TYPE PPI_TASKS[4];
-	// Registers
-	volatile uint32_t RESERVED0[312];
-	volatile uint32_t CHEN;
-	volatile uint32_t CHENSET;
-	volatile uint32_t CHENCLR;
-	volatile uint32_t RESERVED1;
-	volatile PPI_CHANNEL_TYPE PPI_CH[16];
-	volatile uint32_t RESERVED2[156];
-	volatile uint32_t CHG[4];
+    volatile PPI_TASKS_TYPE PPI_TASKS[6]; // Tasks for Chn Groups (0-5)
+    volatile uint32_t RESERVED0[308];     // 308 words to 0x500
+    
+    volatile uint32_t CHEN;               // Channel enable register        [w]
+    volatile uint32_t CHENSET;            // Channel enable set register    [rw]
+    volatile uint32_t CHENCLR;            // Channel enable clear register  [rw]
+    volatile uint32_t RESERVED1[1];       // 1 word to 0x510
+
+    volatile PPI_CHANNEL_TYPE PPI_CH[20]; // PPI Chns (0-19)
+    volatile uint32_t RESERVED2[148];     // 148 words to 0x800
+    
+    volatile uint32_t CHG[6];             // Channel Groups (0-5)
+    volatile uint32_t RESERVED3[62];      // 62 words to 0x910
+    
+    volatile uint32_t FORK_TEP[32];       // Fork Task End Points (0-31)
 } NRF_PPI_REG;
 
 #endif
